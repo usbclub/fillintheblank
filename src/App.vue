4a24e78854d3cef2ea315e38e88c91163ed0e2e2
Norm O'Hagan <whosnorman@gmail.com>
2023-08-30 09:20:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Hello />
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+export default {
+  name: "App",
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+body,
+html {
+  height: 100%;
+  background: #BDC0B6;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
